deb7995047d2c4033eba3238e0617d853249d520
Muhammad Garuda <muhammadgaruda55@yahoo.com>
2021-12-18 00:08:15 +0700

    Initial Commit

---
+++ redirect.js
@@ -0,0 +1,5 @@
+function Redirect() {
+    window.location="https://discord.com/api/oauth2/authorize?client_id=915137422415781919&permissions=534790011728&scope=bot";
+  }
+ 
+ setTimeout('Redirect()', 1000);
