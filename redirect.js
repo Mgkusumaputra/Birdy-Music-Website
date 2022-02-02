d7224e1fa39f782ed13cae16b7b36fe354b32444
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-02-02 10:45:37 +0700

    Fixing redirect link

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 function Redirect() {
-    window.location="https://discord.com/api/oauth2/authorize?client_id=915137422415781919&permissions=535058447184&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkUgH2zUzfD&response_type=code&scope=bot%20rpc";
+    window.location="https://discord.com/api/oauth2/authorize?client_id=915137422415781919&permissions=535058447184&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkUgH2zUzfD&response_type=code&scope=bot%20identify";
   }
  
  setTimeout('Redirect()', 800);
