f34f499c0a2c414dd25c44024a1a3950c88ec48e
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-02-08 19:07:03 +0700

    Make 404 Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
-  content: ["/Pages/*.{html, js}"],
+  content: ["./Pages/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#8BE1FB',
+        'secondary': '#36B4E1',
+        'tersier': '#123D4C',
+        'Whites': '#F6F6F6',
+        'Blacks': '#292929',
+        'dark': '#040D11'
+      },
+      fontFamily: {
+        'default': ['Rubik', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
