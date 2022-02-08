4bf5b7e4ec287573ff889ed77d24a71d4cd2421b
Mgkusumaputra <muhammadgaruda55@yahoo.com>
2022-02-08 17:55:17 +0700

    fixing file structure

--- redirect.js
+++
@@ -1,5 +0,0 @@
-function Redirect() {
-    window.location="https://discord.com/api/oauth2/authorize?client_id=915137422415781919&permissions=535058447184&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkUgH2zUzfD&response_type=code&scope=bot%20identify";
-  }
- 
- setTimeout('Redirect()', 800);
