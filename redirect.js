817449da61701fbadeaad772f6443cc9046521fb
Muhammad Garuda Kusuma P <83939234+Mgkusumaputra@users.noreply.github.com>
2022-05-15 15:19:29 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 function Redirect() {
-    window.location="https://discord.com/api/oauth2/authorize?client_id=915137422415781919&permissions=535058447184&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkUgH2zUzfD&response_type=code&scope=bot%20identify";
+    window.location="https://discord.com/oauth2/authorize?client_id=915137422415781919&permissions=535058446672&redirect_uri=https%3A%2F%2Fdiscord.gg%2FkUgH2zUzfD&response_type=code&scope=bot%20identify";
   }
  
  setTimeout('Redirect()', 800);
